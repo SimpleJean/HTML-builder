109b9bcf5d1cd8042fc7df49f4c77f93a1b465cd
Jean <zhan.yessim@gmail.com>
2022-05-25 23:28:06 +0600

    fix: fixed 5 task, now it's work like technical task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,27 +4,44 @@ const path = require('path');
 const pathToDir = path.join(__dirname, 'styles');
 const pathToDist = path.join(__dirname, 'project-dist/bundle.css');
 
-fs.readdir(pathToDir, (err, files) => {
-  if (err) {
-    console.log(err);
-  } else {
-    files.map((file) => {
-      if (path.extname(file) == '.css') {
-        let readStream = fs.createReadStream(
-          path.join(pathToDir, file),
-          'utf8',
-          'a+',
-          (err) => {
-            if (err) throw console.log(err);
-          }
-        );
-        readStream.on('data', (chunk) => {
-          fs.appendFile(pathToDist, chunk, (err) => {
-            if (err) throw console.log(err);
-            console.log('File appended');
+const createBundleCss = () => {
+  fs.readdir(pathToDir, (err, files) => {
+    if (err) {
+      console.log(err);
+    } else {
+      files.map((file) => {
+        if (path.extname(file) == '.css') {
+          let readStream = fs.createReadStream(
+            path.join(pathToDir, file),
+            'utf8',
+            'a+',
+            (err) => {
+              if (err) throw console.log(err);
+            }
+          );
+          readStream.on('data', (chunk) => {
+            fs.appendFile(pathToDist, chunk, (err) => {
+              if (err) throw console.log(err);
+              console.log('File appended');
+            });
           });
-        });
-      }
-    });
+        }
+      });
+    }
+  });
+};
+createBundleCss();
+
+fs.watch(pathToDir, (eventType, filename) => {
+  if (eventType == 'rename') {
+    console.log('File changed');
+    clearFile();
   }
 });
+
+function clearFile() {
+  fs.unlink(pathToDist, (err) => {
+    if (err) throw console.log(err);
+    createBundleCss();
+  });
+}
